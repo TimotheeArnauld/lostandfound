6f5e76555a9a22d8866fcc3300479cc547c5718c
Yukenigon <timothee.arnauld1@gmail.com>
2017-05-23 14:23:59 +0200

    Fading animation

--- lostandfound/LoginViewController.swift
+++ lostandfound/LoginViewController.swift
@@ -1,13 +1,6 @@
-//
-//  LoginViewController.swift
-//  lostandfound
-//
-//  Created by Timothée Arnauld on 16/05/2017.
-//  Copyright © 2017 Lost And Found. All rights reserved.
-//
-
 import UIKit
 
+import Foundation
 import Firebase
 import GoogleSignIn
 import FBSDKCoreKit
@@ -16,12 +9,41 @@ import FBSDKLoginKit
 class LoginViewController: UIViewController, GIDSignInUIDelegate {
     @IBOutlet weak var signInFB: FBSDKLoginButton!
     @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    var idImage = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         GIDSignIn.sharedInstance().uiDelegate = self
-        //GIDSignIn.sharedInstance().signIn()
+        signInButton.style = GIDSignInButtonStyle.wide
         
+        let facebook = NSAttributedString(string: "Continuer avec Facebook")
+        self.signInFB.setAttributedTitle(facebook, for: self.signInFB.state)
+        
+        var _ = Timer.scheduledTimer(timeInterval: 10.0,
+                                     target: self,
+                                     selector: #selector(changeBackground),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func changeBackground(id : Any){
+        var to_ : UIImage
+        if(idImage == 1){
+            to_ = UIImage(named:"Wallpaper_2")!
+            idImage = 2
+        }else{
+            to_ = UIImage(named:"Wallpaper")!
+            idImage = 1
+        }
+        
+        UIView.transition(with: self.backgroundImage,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve,
+                          animations: {self.backgroundImage.image = to_},
+                          completion: nil)
     }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result:
@@ -30,6 +52,5 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
             print(error.localizedDescription)
             return
         }
-        // ...
     }
 }
