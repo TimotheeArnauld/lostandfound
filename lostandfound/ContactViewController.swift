538b975ed63571fba2f30bada1c85c29fb23dfc9
Yukenigon <timothee.arnauld1@gmail.com>
2017-05-23 14:38:17 +0200

    Add new files -> Contacts, Lost, Found controllers

---
+++ lostandfound/ContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactViewController.swift
+//  lostandfound
+//
+//  Created by Timothée Arnauld on 23/05/2017.
+//  Copyright © 2017 Lost And Found. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
